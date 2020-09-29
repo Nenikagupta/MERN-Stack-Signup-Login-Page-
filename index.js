924a96e0360bcee8d3d58ac2fb270edaec0e7a91
Nenikagupta <49594853+Nenikagupta@users.noreply.github.com>
2020-09-30 00:36:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,24 +8,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
-//some styling
-
-
-
-const selectElement=(s) =>document.querySelector(s);
-const navLinks=document.querySelectorAll(".nav-link");
-
-selectElement(".burger-menu-icon").addEventListener("click", () =>{
-  selectElement(".nav-list").classList.toggle("active");
-  selectElement(".burger-menu-icon").classList.toggle("toggle");
-
-  navLinks.forEach((link, index) => {
-    if(link.style.animation){
-      link.style.animation=""
-    }else{
-      link.style.animation="navLinkAnimate 2.4s ease forwards 0.2s";
-    }
-  })
-
-});
